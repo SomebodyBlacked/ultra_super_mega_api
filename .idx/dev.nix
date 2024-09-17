7fbd61aa06da012d87218a6009485396f6e97987
oversanzero <oversanzero@gmail.com>
2024-09-17 15:24:46 +0000

    change workspace config

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,19 +1,15 @@
 { pkgs, ... }: {
   channel = "stable-23.11";
-  packages = [
-    pkgs.bun
-  ];
+  packages = [ pkgs.bun ];
+  services.mysql.enable = true;
+  services.mysql.package = pkgs.mysql80;
   idx = {
     extensions = [
       "castrogusttavo.symbols"
       "emroussel.atomize-atom-one-dark-theme"
       "esbenp.prettier-vscode"
       "oven.bun-vscode"
+      "rangav.vscode-thunder-client"
     ];
-    workspace = {
-      onCreate = {
-        default.openFiles = [ ".idx/dev.nix" "README.md" ];
-      };
-    };
   };
 }
