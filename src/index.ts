f69e761cf4e4ca884ba08f55b94a778901234c77
oversanzero <oversanzero@gmail.com>
2024-09-19 04:41:34 +0000

    add new routes in src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia()
-  .get("/", () => "Hello Elysia")
-  .get("/hello", () => "Hello")
-  .listen(3000);
+const app = new Elysia();
+
+app.get("/", function () {
+  return "Hello Elysia";
+});
+
+app.get("/hello", function () {
+  return "Hello";
+});
+
+app.get("/world", function () {
+  return "World";
+});
+
+app.get("/hello/:name", function ({ params }) {
+  return `Hello ${params.name}`;
+});
+
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
